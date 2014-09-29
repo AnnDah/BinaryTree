aa1b4a0990aa5ca5d0f3e0cc3ecf722d8935daf7
Annika Magnusson <annika.h.magnusson@gmail.com>
2014-09-29 14:14:15 +0200

    Entered code from teacher

---
+++ BinaryTreeAssignment/BinaryTree.cs
@@ -0,0 +1,46 @@
+﻿//BinaryTreeAssignment.cs
+//Created by Annika Magnusson 2014-09-29
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeAssignment
+{
+    class BinaryTree
+    {
+        private TreeNode root = null;
+        private int size = 0;
+        private int count = 0;
+
+        public TreeNode Insert(int value)
+        {
+            TreeNode node = new TreeNode(value);
+            try
+            {
+                if (root == null)
+                {
+                    root = node;
+                }
+                else
+                {
+                    Add(node, ref root);
+                    count++;
+                }
+                return node; //in the example from teacher this is inside else
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Value allready exists");
+                return null;
+            }
+        }
+
+        public void Add(TreeNode node, ref TreeNode tree)
+        {
+
+        }
+    }
+}
---
+++ BinaryTreeAssignment/TreeNode.cs
@@ -0,0 +1,25 @@
+﻿//BinaryTreeAssignment.cs
+//Created by Annika Magnusson 2014-09-29
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeAssignment
+{
+    class TreeNode
+    {
+        private int value;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
